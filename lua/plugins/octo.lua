a224d492923d4d830b70e1308ce5ba099b40a9bd
audishosdd <audisho.sada@doordash.com>
2024-01-26 15:32:01 -0500

    adds octo for GitHub integrations

---
+++ lua/plugins/octo.lua
@@ -0,0 +1,16 @@
+-- https://github.com/pwntester/octo.nvim
+-- GitHub issues & PR integrations
+return {
+  {
+    "pwntester/octo.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      -- OR 'ibhagwan/fzf-lua',
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup()
+    end,
+  },
+}
