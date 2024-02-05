6e4ae5807ef37bfe4b7c407105ff83354c0dbab7
audishos <audisho.sada@gmail.com>
2024-02-05 17:54:40 -0500

    merge colorscheme config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+  { "dracula/vim" },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
