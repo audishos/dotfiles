529f31168d57f29aa27b56f778d7a2bebbceb084
audishos <audisho.sada@gmail.com>
2024-02-07 00:46:37 -0500

    adds some custom css to waybar

--- home/waybar.nix
+++ home/waybar.nix
@@ -1,6 +1,47 @@
 { catppuccinWaybar, ... }:
 {
-  programs.waybar.enable = true;
+  programs.waybar = {
+    enable = true;
+    style = ''
+      @import "mocha.css";
+
+      * {
+        font-family: FiraCode Nerd Font Mono;
+        font-size: 1.1rem;
+      }
+
+      .modules-left,
+      .modules-center,
+      .modules-right {
+        background-color: @base;
+        margin: 0 16px;
+        padding: 0 16px;
+        border-radius: 24px;
+      }
+
+      window#waybar {
+        color: @text;
+        background-color: transparent;
+      }
+
+      #workspaces button {
+        color: @text;
+        border-radius: 32px;
+      }
+
+      #workspaces button.focused,
+      #workspaces button.active,
+      #workspaces button.visible {
+        background-color: @green;
+        color: @crust;
+        box-shadow: inset 0 -4px @maroon;
+      }
+
+      #idle_inhibitor {
+        font-size: 2rem;
+      }
+    '';
+  };
 
   # Copy catppuccin css files to config dir
   xdg.configFile."waybar" = {
