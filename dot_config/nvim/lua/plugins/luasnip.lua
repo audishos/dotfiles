c0943544289774ed1ffb81c8d872b2b5150d2a42
audishos <audisho.sada@gmail.com>
2024-04-16 20:43:15 -0400

    supertab & telescope-fzf-native

---
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      -- disables <tab> & <s-tab> for supertab compatibility
+      -- see cmp.lua
+      return {}
+    end,
+  },
+}
