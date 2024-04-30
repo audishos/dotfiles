410f8dc3c6765e4a946884a32006f79b4c1fb625
audishos <audisho.sada@gmail.com>
2024-04-30 19:39:44 -0400

    qpdfview default for application/pdf

--- nixos/home/default-applications.nix
+++ nixos/home/default-applications.nix
@@ -4,6 +4,7 @@
 
     defaultApplications = {
       "inode/directory" = "org.gnome.Nautilus.desktop";
+      "application/pdf" = "qpdfview.desktop";
       "x-scheme-handler/ror2mm" = "r2modman.desktop";
     };
   };
