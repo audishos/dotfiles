199029fe90db7e9298fe7953c9e6883c0080e8b8
audishosdd <audisho.sada@doordash.com>
2024-07-05 16:32:18 -0400

    cleanup & alignment w/ latest lazyvim update

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -4,14 +4,13 @@ return {
     dependencies = { "hrsh7th/cmp-emoji" },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      -- SUPERTAB!
+      -- supertab
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
@@ -19,10 +18,10 @@ return {
           if cmp.visible() then
             -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
             cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
           elseif has_words_before() then
             cmp.complete()
           else
@@ -32,8 +31,10 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
           else
             fallback()
           end
@@ -42,13 +43,6 @@ return {
 
       -- emojis 🎉
       table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      -- disables <tab> & <s-tab> for supertab compatibility
-      return {}
     end,
   },
 }
--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -2,6 +2,26 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = { "nvim-neotest/neotest-jest" },
-    opts = { adapters = { "neotest-jest" } },
+    opts = {
+      adapters = { "neotest-jest" },
+      -- Example for loading neotest-golang with a custom config
+      -- adapters = {
+      --   ["neotest-golang"] = {
+      --     go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+      --     dap_go_enabled = true,
+      --   },
+      -- },
+      status = { virtual_text = true },
+      output = { open_on_run = true },
+      quickfix = {
+        open = function()
+          if LazyVim.has("trouble.nvim") then
+            require("trouble").open({ mode = "quickfix", focus = false })
+          else
+            vim.cmd("copen")
+          end
+        end,
+      },
+    },
   },
 }
