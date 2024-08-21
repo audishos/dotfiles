b7add7b3976bc0ff2cbe66d9c8ec4c8d547076cc
audishosdd <audisho.sada@doordash.com>
2024-08-21 15:38:19 -0400

    lsp server fixes
    - eslint in yarn 1 monorepo package
    - nil_ls errors on non-nix projects

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,9 +1,20 @@
+local util = require("lspconfig.util")
+
+local get_eslint_root_in_monorepo = function(fname)
+  return util.root_pattern("package.json", "tsconfig.json")(fname) or util.root_pattern(".git")(fname)
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        nil_ls = {},
+        eslint = {
+          root_dir = get_eslint_root_in_monorepo,
+        },
+        nil_ls = {
+          root_dir = util.root_pattern("flake.nix"),
+        },
       },
     },
   },
