636b49a344890b746de206c4a544427174850cd1
audishos <audisho.sada@gmail.com>
2024-11-16 09:55:16 -0500

    spotify_player enables audio normalization

--- nixos/home/spotify.nix
+++ nixos/home/spotify.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   programs = {
     spotify-player = {
       enable = true;
-      package = pkgs.spotify-player.override { withAudioBackend = "pulseaudio"; };
+      package = pkgs.spotify-player.override {withAudioBackend = "pulseaudio";};
 
       settings = {
         client_id = "cdf763f761f241858dbaa7cf4ebf1d3b";
+        device.normalization = true;
       };
     };
   };
