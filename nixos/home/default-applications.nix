4f89ff24242ba2170d982e0a064e2d176274a6b9
audishos <audisho.sada@gmail.com>
2024-11-16 10:14:35 -0500

    fixes build failure
    adds telegram to default apps, removes mimeapps file overwritten by
    telegram, and rebuilds

--- nixos/home/default-applications.nix
+++ nixos/home/default-applications.nix
@@ -3,20 +3,22 @@
     enable = true;
 
     defaultApplications = {
-      "inode/directory" = "org.kde.dolphin.desktop";
       "application/pdf" = "qpdfview.desktop";
-      "x-scheme-handler/ror2mm" = "r2modman.desktop";
-      "x-scheme-handler/http" = "firefox-devedition.desktop";
-      "x-scheme-handler/https" = "firefox-devedition.desktop";
-      "x-scheme-handler/chrome" = "firefox-devedition.desktop";
-      "text/html" = "firefox-devedition.desktop";
       "application/x-extension-htm" = "firefox-devedition.desktop";
       "application/x-extension-html" = "firefox-devedition.desktop";
       "application/x-extension-shtml" = "firefox-devedition.desktop";
-      "application/xhtml+xml" = "firefox-devedition.desktop";
-      "application/x-extension-xhtml" = "firefox-devedition.desktop";
       "application/x-extension-xht" = "firefox-devedition.desktop    ";
+      "application/x-extension-xhtml" = "firefox-devedition.desktop";
+      "application/xhtml+xml" = "firefox-devedition.desktop";
+      "inode/directory" = "org.kde.dolphin.desktop";
+      "text/html" = "firefox-devedition.desktop";
       "video/mp4" = "mpv.desktop";
+      "x-scheme-handler/chrome" = "firefox-devedition.desktop";
+      "x-scheme-handler/http" = "firefox-devedition.desktop";
+      "x-scheme-handler/https" = "firefox-devedition.desktop";
+      "x-scheme-handler/ror2mm" = "r2modman.desktop";
+      "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+      "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
     };
   };
 }
