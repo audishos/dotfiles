7052de2e40366798cf49a267b2abf0e2e02f5777
audishosdd <audisho.sada@doordash.com>
2024-11-19 15:28:57 -0500

    adds pomodoro timer to neovim

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      sections = {
+        lualine_x = {
+          function()
+            local ok, pomo = pcall(require, "pomo")
+            if not ok then
+              return ""
+            end
+
+            local timer = pomo.get_first_to_finish()
+            if timer == nil then
+              return ""
+            end
+
+            return "󰄉 " .. tostring(timer)
+          end,
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+      },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/pomo.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "epwalsh/pomo.nvim",
+    version = "*", -- Recommended, use latest release instead of latest commit
+    lazy = true,
+    cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
+    dependencies = {
+      "nvim-lualine/lualine.nvim",
+    },
+    opts = {
+      notifiers = {
+        -- The "Default" notifier uses 'vim.notify' and works best when you have 'nvim-notify' installed.
+        {
+          name = "Default",
+          opts = {
+            -- With 'nvim-notify', when 'sticky = true' you'll have a live timer pop-up
+            -- continuously displayed. If you only want a pop-up notification when the timer starts
+            -- and finishes, set this to false.
+            sticky = false,
+
+            -- Configure the display icons:
+            title_icon = "󱎫",
+            text_icon = "󰄉",
+            -- Replace the above with these if you don't have a patched font:
+            -- title_icon = "⏳",
+            -- text_icon = "⏱️",
+          },
+        },
+      },
+    },
+  },
+}
