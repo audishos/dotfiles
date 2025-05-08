b36e66c593aaddb79b95eaf33a88b03a864d9ded
audishos <audisho.sada@gmail.com>
2025-05-08 18:45:09 -0400

    nixos: enables sway cursor config

--- nixos/home/cursors.nix
+++ nixos/home/cursors.nix
@@ -1,9 +1,13 @@
 {pkgs, ...}: {
   home.pointerCursor = {
     enable = true;
-    # sway.enable = true;
+    sway.enable = true;
 
     name = "mochaGreen";
     package = pkgs.catppuccin-cursors.mochaGreen;
   };
+
+  # sway config generation contains a dependency on this value
+  # https://github.com/nix-community/home-manager/blob/master/modules/config/home-cursor.nix#L251
+  gtk.cursorTheme.size = 32;
 }
