2e8c5ac7ab5366a8d0501b9e730380b9698f7cb9
audishos <audisho.sada@gmail.com>
2025-08-27 11:39:05 -0400

    nixos: builds spotify_player for gstreamer
    instead of pulse audio - needs testing once app api issues are resolved

--- nixos/home/spotify.nix
+++ nixos/home/spotify.nix
@@ -2,7 +2,7 @@
   programs = {
     spotify-player = {
       enable = true;
-      package = pkgs.spotify-player.override {withAudioBackend = "pulseaudio";};
+      package = pkgs.spotify-player.override {withAudioBackend = "gstreamer";};
 
       settings = {
         client_id = "cdf763f761f241858dbaa7cf4ebf1d3b";
