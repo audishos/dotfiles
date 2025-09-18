1322656e139f9d29625550f32107ffadf87510d5
audishos <audisho.sada@gmail.com>
2025-09-18 11:16:15 -0400

    nixos: fixes spotify_player authorization
    also removes custom audio backend which is no longer needed

--- nixos/home/spotify.nix
+++ nixos/home/spotify.nix
@@ -2,9 +2,9 @@
   programs = {
     spotify-player = {
       enable = true;
-      package = pkgs.spotify-player.override {withAudioBackend = "gstreamer";};
 
       settings = {
+        login_redirect_uri = "http://127.0.0.1:8989/login";
         client_id = "cdf763f761f241858dbaa7cf4ebf1d3b";
         device = {
           volume = 100;
