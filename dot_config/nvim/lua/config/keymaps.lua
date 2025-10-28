06e43b1b8439702d656171ef43b16b1e2b547835
audishos <audisho.sada@gmail.com>
2025-10-28 09:34:34 -0400

    nvim: add keymap for new zk journal

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
+local Zk = require("zk.commands")
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
@@ -7,3 +8,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>KittyNavigateDown<cr>")
 vim.keymap.set("n", "<C-k>", "<cmd>KittyNavigateUp<cr>")
 vim.keymap.set("n", "<C-l>", "<cmd>KittyNavigateRight<cr>")
 vim.keymap.set("n", "<C-h>", "<cmd>KittyNavigateLeft<cr>")
+
+-- ZK
+vim.keymap.set("n", "<leader>z", "", { desc = "Zk" })
+vim.keymap.set("n", "<leader>zj", function()
+  Zk.get("ZkNew")({ group = "daily", dir = "journal/daily" })
+end, { desc = "New journal entry" })
