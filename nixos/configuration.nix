7d4973af6f74051411f68b57bcf50068d495ff1e
audishos <audisho.sada@gmail.com>
2025-11-13 08:46:35 -0500

    nixos: opens ports for kde connect

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -224,6 +224,23 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPortRanges = [
+      # KDE Connect
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = [
+      # KDE Connect
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
