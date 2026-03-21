6ad5137425a645b862ce6f1ea78d895147502206
audishos <audisho.sada@gmail.com>
2026-03-21 19:50:52 -0400

    nvim: removes deprecated configs
    - kitty integrations
    - lsp override
    - treesitter install all (for NixOS)
    - updates

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,12 +3,6 @@ local Zk = require("zk.commands")
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Kitty Navigate Splits
-vim.keymap.set("n", "<C-j>", "<cmd>KittyNavigateDown<cr>")
-vim.keymap.set("n", "<C-k>", "<cmd>KittyNavigateUp<cr>")
-vim.keymap.set("n", "<C-l>", "<cmd>KittyNavigateRight<cr>")
-vim.keymap.set("n", "<C-h>", "<cmd>KittyNavigateLeft<cr>")
-
 -- ZK
 vim.keymap.set("n", "<leader>z", "", { desc = "Zk" })
 vim.keymap.set("n", "<leader>zj", function()
