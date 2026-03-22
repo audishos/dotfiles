a9967375804205c846594082671c5e83d09aa086
audishos <audisho.sada@gmail.com>
2026-03-22 00:16:24 -0400

    mango config updates
    - disables some conflicting bindings
    - adds env var
    - sleep keepassxc and nextcloud before launch for waybar compatibility

--- dot_config/mango/executable_autostart.sh
+++ dot_config/mango/executable_autostart.sh
@@ -8,9 +8,6 @@ set +e # continue script execution on failure
 # bars
 waybar -c ~/.config/mango/waybar/config.jsonc >/dev/null 2>&1 &
 
-# passwords
-keepass-xc >/dev/null 2>&1 &
-
 # wallpaper
 swaybg -i ~/Nextcloud/aizhai_bridge-1.jpg -m fill >/dev/null 2>&1 &
 
@@ -19,3 +16,9 @@ swaybg -i ~/Nextcloud/aizhai_bridge-1.jpg -m fill >/dev/null 2>&1 &
 
 # inhibit idle
 wayland-pipewire-idle-inhibit >/dev/null 2>&1 &
+
+# passwords
+sleep 10 keepass-xc >/dev/null 2>&1 &
+
+# file sync
+sleep 10 nextcloud >/dev/null 2>&1 &
