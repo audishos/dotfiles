2db201d43c58cd9729af5445e24f9236f62c479f
audishos <audisho.sada@gmail.com>
2026-03-23 11:16:34 -0400

    adds chezmoi.nvim
    auto-run apply on config changes

---
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "xvzc/chezmoi.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("chezmoi").setup({
+        -- your configurations
+        watch = true,
+      })
+      --  e.g. ~/.local/share/chezmoi/*
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+        callback = function(ev)
+          local bufnr = ev.buf
+          local edit_watch = function()
+            require("chezmoi.commands.__edit").watch(bufnr)
+          end
+          vim.schedule(edit_watch)
+        end,
+      })
+    end,
+    lazy = false,
+  },
+}
