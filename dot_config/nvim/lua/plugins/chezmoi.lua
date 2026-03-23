0c18cb4dcc308230bd3ace0930a3d3092176b6bc
audishos <audisho.sada@gmail.com>
2026-03-23 11:23:56 -0400

    chezmoi.nvim lazy invocation
    moves watch function outside of setup
    lazy invokes chezmoi.nvim when triggered

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,3 +1,16 @@
+-- Watch for changes in chezmoi path
+-- Enables lazy invocation of chezmoi.nvim
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+  callback = function(ev)
+    local bufnr = ev.buf
+    local edit_watch = function()
+      require("chezmoi.commands.__edit").watch(bufnr)
+    end
+    vim.schedule(edit_watch)
+  end,
+})
+
 return {
   {
     "xvzc/chezmoi.nvim",
@@ -5,20 +18,7 @@ return {
     config = function()
       require("chezmoi").setup({
         -- your configurations
-        watch = true,
-      })
-      --  e.g. ~/.local/share/chezmoi/*
-      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
-        callback = function(ev)
-          local bufnr = ev.buf
-          local edit_watch = function()
-            require("chezmoi.commands.__edit").watch(bufnr)
-          end
-          vim.schedule(edit_watch)
-        end,
       })
     end,
-    lazy = false,
   },
 }
